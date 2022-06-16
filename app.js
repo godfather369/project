ad1a57a07559fe1a6a3d662b624ccf10348257e3
Sravan Kotcherla <sravankotcherla@Sravans-MacBook-Pro.local>
2022-06-16 15:37:26 +0530

    Analog clock is working

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 //Function for Analog Clock
 
-let rst2=document.querySelector('#reset2');
+let arm=document.querySelector('#hand');
+let rst=document.querySelector("#reset");
+let startingTime=15;
+arm.classList.add('anim');
+arm.style.animationDuration=startingTime+'s';
 
-const analogClock=rst2.addEventListener('click',()=>{
-    const arm=document.querySelector('#hand');
-    arm.style.backgroundColor='red';
-    arm.style.transform="translateY(5px) translateX(73px) rotate(1turn)"
-    arm.style.transition='transform 10s linear'
-})
+function rotor(){
+    arm.classList.remove('anim');
+    void arm.offsetWidth;
+    arm.classList.add('anim');
+}
+
+rst.addEventListener('click', rotor)
+
