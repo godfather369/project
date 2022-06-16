fbbf22fdd7565d0d3ae082387ca5363b9629a390
NITISH KUMAR <nitish@NITISHs-MacBook-Pro.local>
2022-06-16 13:12:33 +0530

    created digital countdown timer

---
+++ app.js
@@ -0,0 +1,25 @@
+const startingTime = 15;
+
+let time = startingTime;
+
+let reset = document.querySelector(".reset");
+
+const timer = document.getElementById("countdownTimer");
+
+let id = setInterval(updateTimer, 1000);
+
+function updateTimer() {
+  if (time < 10) {
+    time = "0" + time;
+  }
+  timer.innerHTML = `${time}`;
+  time--;
+  if (time < 0) {
+    time = startingTime;
+  }
+}
+
+reset.addEventListener("click", () => {
+  clearInterval(setInterval);
+  time = startingTime;
+});
