e11c77f328648675b4298a05dabccc2fa30d478a
Sravan Kotcherla <sravankotcherla@Sravans-MacBook-Pro.local>
2022-06-21 15:07:57 +0530

    JS code is organised

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
-let userInput = document.getElementById("entertime");
+const numberOfImages = 3;
+
 let startingTime = 10;
 let time = startingTime;
-let rstDigital = document.querySelector(".reset");
+let indexNav = 0;
+let indexImg = 0;
+
+const userInput = document.getElementById("entertime");
 const timer = document.getElementById("countdownTimer");
+const carouselNavDot = document.querySelectorAll(".carousel-nav-dot");
+const carouselImage = document.querySelectorAll(".carousel-image");
+const rstCarousel = document.querySelector("#carousel-reset-button");
+const rstDigital = document.querySelector(".reset");
+const arm = document.querySelector("#hand");
+const rstAnalog = document.querySelector("#reset");
 
-let arm = document.querySelector("#hand");
-let rstAnalog = document.querySelector("#reset");
+const timerId = setInterval(updateTimer, 1000);
+
 arm.classList.add("anim");
-
-const numberOfImages=3;
-let rstCarousel = document.querySelector("#carousel-reset-button");
-let carouselNavDot=document.querySelectorAll(".carousel-nav-dot");
-let indexNav=0;
-let carouselImage=document.querySelectorAll(".carousel-image");
-let indexImg=0;
-
 timer.textContent = `${time}`;
 
-let timerId= setInterval(updateTimer, 1000);
 
-start.addEventListener("click", startFunc );
-rstAnalog.addEventListener("click", resetFunc)
+start.addEventListener("click", startFunc);
+rstAnalog.addEventListener("click", resetFunc);
 rstCarousel.addEventListener("click", resetFunc);
 rstDigital.addEventListener("click", resetFunc);
 
@@ -37,56 +38,54 @@ function updateTimer() {
 }
 
 function rotateClock() {
-  arm.style.animationDuration = userInput.value.concat('s');
+  arm.style.animationDuration = userInput.value.concat("s");
   arm.classList.remove("anim");
   void arm.offsetWidth;
   arm.classList.add("anim");
 }
 
 function startFunc() {
-  if(userInput.value===""){
-    startingTime=10;
-  }else{
-    startingTime=parseInt(userInput.value);
-    carouselImage.forEach(carouselImageObject => {
+  if (userInput.value === "") {
+    startingTime = 10;
+  } else {
+    startingTime = parseInt(userInput.value);
+    carouselImage.forEach((carouselImageObject) => {
       carouselImageObject.classList.remove("current");
       carouselImageObject.classList.remove("next");
-      carouselImageObject.style.animationDuration=userInput.value.concat('s');
+      carouselImageObject.style.animationDuration = userInput.value.concat("s");
     });
   }
   resetFunc();
   userInput.value = "";
 }
 
-function carouselNavDotAnimation(){
+function carouselNavDotAnimation() {
   carouselNavDot[indexNav].classList.remove("selected");
   indexNav++;
-  if(indexNav===numberOfImages){
-    indexNav=0;
+  if (indexNav === numberOfImages) {
+    indexNav = 0;
   }
   carouselNavDot[indexNav].classList.add("selected");
 }
 
-function carouselImageAnimation(){
+function carouselImageAnimation() {
   carouselImage[indexImg].classList.remove("current");
-  if(indexImg==numberOfImages-1){
+  if (indexImg == numberOfImages - 1) {
     carouselImage[0].classList.remove("next");
+  } else {
+    carouselImage[indexImg + 1].classList.remove("next");
   }
-  else{
-    carouselImage[indexImg+1].classList.remove("next");
-  }
-  
+
   indexImg++;
-  if(indexImg===numberOfImages){
-    indexImg=0;
+  if (indexImg === numberOfImages) {
+    indexImg = 0;
   }
-  
+
   carouselImage[indexImg].classList.add("current");
-  if(indexImg===numberOfImages-1){
+  if (indexImg === numberOfImages - 1) {
     carouselImage[0].classList.add("next");
-  }
-  else{
-    carouselImage[indexImg+1].classList.add("next");
+  } else {
+    carouselImage[indexImg + 1].classList.add("next");
   }
 }
 
@@ -96,16 +95,18 @@ function resetFunc() {
   timer.textContent = time;
   rotateClock();
   timerId = setInterval(updateTimer, 1000);
-  carouselNavDot.forEach(carouselNavDotObject => {
+  carouselNavDot.forEach((carouselNavDotObject) => {
     carouselNavDotObject.classList.remove("selected");
   });
   carouselNavDot[0].classList.add("selected");
-  indexNav=0;
-  carouselImage.forEach(carouselImageObject => {
+  indexNav = 0;
+  carouselImage.forEach((carouselImageObject) => {
     carouselImageObject.classList.remove("current");
     carouselImageObject.classList.remove("next");
   });
   carouselImage[0].classList.add("current");
   carouselImage[1].classList.add("next");
-  indexImg=0;
+  indexImg = 0;
 }
+
+
